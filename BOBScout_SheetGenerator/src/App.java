259aea4063e410a6bc92b5078115661e3c6e9735
kyleh8892 <kyleh8892@gmail.com>
2022-01-06 01:04:58 -0500

    Made variables easy to change. Changed program to read existing file instead of making a new one.

--- BOBScout_SheetGenerator/src/App.java
+++ BOBScout_SheetGenerator/src/App.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,21 +15,38 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        TBA.setAuthToken("wi5cNwKSrOnIJeeePTFtd9fR82gDmRgPBZj7dQLwp169sMVnv1UYudtaLSxjxEKz");
-        TBA tba = new TBA();
-        Team[] teamList = tba.getEventTeams("2022migul");
 
-        HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("Sample sheet");
+    private final static int ID_ROW = 0;
+    private final static int NUMBER_COLUMN = 3;
+    private final static int NAME_COLUMN = 4;
+
+    private final static String AUTHTOKEN = "wi5cNwKSrOnIJeeePTFtd9fR82gDmRgPBZj7dQLwp169sMVnv1UYudtaLSxjxEKz";
+    private final static String EVENTKEY = "2022migul";
+    private final static String SCOUTINGFILEPATHNAME = "D:\\2022 Robotics\\2022 Scout Template.xls";
+    public static void main(String[] args) throws Exception {
+        TBA.setAuthToken(AUTHTOKEN);
+        TBA tba = new TBA();
+        Team[] teamList = tba.getEventTeams(EVENTKEY);
+
+        File scoutingFile = null;
+        FileInputStream inStream = null;
+        try{
+            scoutingFile = new File(SCOUTINGFILEPATHNAME);
+            inStream = new FileInputStream(scoutingFile);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } 
+        
+        HSSFWorkbook workbook = new HSSFWorkbook(inStream);
 
         for(Team team : teamList){
-            System.out.println(team.getTeamNumber() + "  " + team.getNickname());
+            //System.out.println(team.getTeamNumber() + "  " + team.getNickname());
             HSSFSheet temp = workbook.cloneSheet(0);
 
-            Row row = temp.createRow(0);
-            Cell name = row.createCell(3);
-            Cell number = row.createCell(4);
+            Row row = temp.createRow(ID_ROW);
+            Cell name = row.createCell(NUMBER_COLUMN);
+            Cell number = row.createCell(NAME_COLUMN);
 
             name.setCellValue(team.getNickname());
             number.setCellValue(team.getTeamNumber());
@@ -36,12 +54,11 @@ public class App {
 
         try {
             FileOutputStream out =
-                new FileOutputStream(new File("D:\\2022 Robotics\\new.xls"));
+                new FileOutputStream(scoutingFile);
             workbook.write(out);
             out.close();
             workbook.close();
             System.out.println("Excel written successfully..");
-        
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
